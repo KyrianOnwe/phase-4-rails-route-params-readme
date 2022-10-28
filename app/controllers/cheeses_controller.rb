2dc42c7437c0843e490e7c3f0ace0a4732f2c005
Kyrian <dr.kyrian.onwe@gmail.com>
2022-10-27 21:06:55 -0500

    commit tree aabac454e5bb7099dcb58677a0eb1a1518dba223

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,11 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def show
+    # find a cheese using url and id, send a json resp using the cheese obj
+    cheese = Cheese.find(params[:id])
+    render json: cheese
+
+  end
+
 end
